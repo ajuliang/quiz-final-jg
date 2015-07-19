db7082c04f34c6f112f260e02234a510fea11a52
Julián González <a.julian.g.30@gmail.com>
2015-07-19 18:55:17 -0500

    Añadir Creditos al proyecto

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res){
+	res.render('author', {autor: 'Julián González', foto: 'foto.jpg'});
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
